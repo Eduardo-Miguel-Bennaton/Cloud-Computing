261dd5d93cd883a7f5b9147f2ed5aadbbb72e740
Eduardo Miguel Bennaton Handal <eduardohandal2@gmail.com>
2024-01-23 21:20:08 +0100

    Medium update
    - Working on AWS issues regarding the development of the Lambda function

---
+++ Lambda/go.mod
@@ -0,0 +1,3 @@
+module Lambda
+
+go 1.21.3
---
+++ Lambda/lambdaFileA.go
@@ -0,0 +1,90 @@
+// processes/processA.go
+package Lambda
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	// "log"
+	// "strings"
+	"github.com/Knetic/govaluate"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+func ProcessA() {
+	// Set your AWS region
+	region := "US East (N. Virginia) us-east-1"
+
+	// Initialize AWS SDK configuration
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
+	if err != nil {
+		fmt.Printf("Error loading AWS SDK config: %v\n", err)
+		return
+	}
+
+	// Create an S3 client
+	client := s3.NewFromConfig(cfg)
+
+	// Specify your S3 bucket and folder
+	bucket := "awscloudfinalproject2024"
+	folder := "Type A/"
+
+	// List objects in the specified folder
+	listInput := &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket),
+		Prefix: aws.String(folder),
+	}
+
+	listOutput, err := client.ListObjectsV2(context.TODO(), listInput)
+	if err != nil {
+		fmt.Printf("Error listing objects in S3: %v\n", err)
+		return
+	}
+
+	// Process each object in the folder
+	for _, object := range listOutput.Contents {
+		// Download the object's content
+		getInput := &s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    object.Key,
+		}
+
+		getOutput, err := client.GetObject(context.TODO(), getInput)
+		if err != nil {
+			fmt.Printf("Error getting object from S3: %v\n", err)
+			return
+		}
+
+		defer getOutput.Body.Close()
+
+		// Read the content of the object
+		fileData, err := ioutil.ReadAll(getOutput.Body)
+		if err != nil {
+			fmt.Printf("Error reading file data: %v\n", err)
+			return
+		}
+
+		// Call the processing algorithm
+		processFileData(fileData)
+	}
+}
+
+func processFileData(fileData []byte) {
+	fmt.Print("Processing file type A:", string(fileData))
+
+	expression, err := govaluate.NewEvaluableExpression(string(fileData))
+	if err != nil {
+		fmt.Printf("Error creating expression: %v\n", err)
+		return
+	}
+
+	result, err := expression.Evaluate(nil)
+	if err != nil {
+		fmt.Printf("Error evaluating expression: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Result of expression: %v\n\n", result)
+}
