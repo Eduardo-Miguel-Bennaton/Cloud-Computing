48b26e813a4fa0cdbb1ba6081bcfcd7b29fa8935
Eduardo Miguel Bennaton Handal <eduardohandal2@gmail.com>
2024-01-23 19:31:15 +0100

    Major update
    - All filetypes are decodable and readable according to their development logic

--- GoLang/processes/processB.go
+++ GoLang/processes/processB.go
@@ -2,31 +2,15 @@ package processes
 
 import (
 	"fmt"
-	"github.com/Knetic/govaluate"
 )
 
 func ProcessB(fileData []byte) {
 	// Reverse the string
 	reversedString := reverseString(string(fileData))
-	fmt.Printf("Processing file type B. Original string: %s, Reversed string: %s\n", string(fileData), reversedString)
-
-	// Pass the reversed string to govaluate
-	expression, err := govaluate.NewEvaluableExpression(reversedString)
-	if err != nil {
-		fmt.Printf("Error creating expression: %v\n", err)
-		return
-	}
-
-	result, err := expression.Evaluate(nil)
-	if err != nil {
-		fmt.Printf("Error evaluating expression: %v\n", err)
-		return
-	}
-
-	fmt.Printf("Result of expression: %v\n\n", result)
+	fmt.Printf("Processing file type B: Original string: %s, Reversed string: %s\n", string(fileData), reversedString)
+	fmt.Println()
 }
 
-// reverseString reverses a string
 func reverseString(s string) string {
 	runes := []rune(s)
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
