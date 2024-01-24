202ff9d356ccee31695515211d537e8682c96763
Eduardo Miguel Bennaton Handal <eduardohandal2@gmail.com>
2024-01-24 18:34:45 +0100

    Minor update
    - Changes in AWS Console (C master lambda complete with worker lambda accomplished)

--- Lambda/processA.py
+++ Lambda/processA.py
@@ -13,13 +13,13 @@ def executeProcess(function_url, payload, specifier):
 
 function_urlMasterA = 'https://pdx3axs6nfkww2hia2irl55bwq0oubzi.lambda-url.us-east-1.on.aws/'
 function_urlMasterB = 'https://wzlndx4dut7c7fqn4i3wgqzbn40fdkra.lambda-url.us-east-1.on.aws/'
-function_urlMasterC = 'https://pdx3axs6nfkww2hia2irl55bwq0oubzi.lambda-url.us-east-1.on.aws/'
+function_urlMasterC = 'https://uwnnbftxftsk735k34dmzkbzva0bwvqb.lambda-url.us-east-1.on.aws/'
 
 payload = {
 }
 
-# executeProcess(function_urlMasterA, payload, "A") # Execution of A Master Thread
+executeProcess(function_urlMasterA, payload, "A") # Execution of A Master Thread
 print("\n")
 executeProcess(function_urlMasterB, payload, "B") # Execution of B Master Thread
 print("\n")
-# executeProcess(function_urlMasterC, payload, "C") # Execution of C Master Thread
+executeProcess(function_urlMasterC, payload, "C") # Execution of C Master Thread
