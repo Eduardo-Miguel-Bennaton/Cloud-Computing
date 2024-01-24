42d5045937a80f1839d4c3e682a7e46e64cad485
Eduardo Miguel Bennaton Handal <eduardohandal2@gmail.com>
2024-01-24 17:30:59 +0100

    Major update
    - Lambda activation functions successfully

--- Lambda/processA.py
+++ Lambda/processA.py
@@ -4,14 +4,13 @@ import json
 function_url = 'https://pdx3axs6nfkww2hia2irl55bwq0oubzi.lambda-url.us-east-1.on.aws/'
 
 payload = {
-    'input': 'CAM-A' # Classify and Modify - A
 }
 
 response = requests.post(function_url, data=json.dumps(payload))
 
 if response.status_code == 200:
-  print("Post sent successfully")
-  print(response.json())
+    print("Post sent successfully")
+    print(response.json())
 else:
-  print("ERROR: Process was not executable")
-  print(f"Reason: {response.status_code}")
+    print("ERROR: Process was not executable")
+    print(f"Reason: {response.status_code}")
