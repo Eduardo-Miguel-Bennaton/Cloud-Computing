c1e2f56c508de59accccb3d2e419447396f00282
Eduardo Miguel Bennaton Handal <eduardohandal2@gmail.com>
2024-01-24 18:12:30 +0100

    Medium update
    - Major changes done to AWS platform console but added a better view of processes inside of the HTTP POST call

--- Lambda/processA.py
+++ Lambda/processA.py
@@ -11,16 +11,15 @@ def executeProcess(function_url, payload, specifier):
         print("ERROR: Process was not executable")
         print(f"Reason: {response.status_code}")
 
-    print("\n")
-
-
 function_urlMasterA = 'https://pdx3axs6nfkww2hia2irl55bwq0oubzi.lambda-url.us-east-1.on.aws/'
-function_urlMasterB = 'https://pdx3axs6nfkww2hia2irl55bwq0oubzi.lambda-url.us-east-1.on.aws/'
+function_urlMasterB = 'https://wzlndx4dut7c7fqn4i3wgqzbn40fdkra.lambda-url.us-east-1.on.aws/'
 function_urlMasterC = 'https://pdx3axs6nfkww2hia2irl55bwq0oubzi.lambda-url.us-east-1.on.aws/'
 
 payload = {
 }
 
-executeProcess(function_urlMasterA, payload, "A") # Execution of A Master Thread
+# executeProcess(function_urlMasterA, payload, "A") # Execution of A Master Thread
+print("\n")
 executeProcess(function_urlMasterB, payload, "B") # Execution of B Master Thread
-executeProcess(function_urlMasterC, payload, "C") # Execution of C Master Thread
+print("\n")
+# executeProcess(function_urlMasterC, payload, "C") # Execution of C Master Thread
