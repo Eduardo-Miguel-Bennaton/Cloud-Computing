b3eac1951a5d7d360a794df7f685df7d0ff1e343
Eduardo Miguel Bennaton Handal <eduardohandal2@gmail.com>
2024-01-24 17:51:54 +0100

    Medium update
    - Improved better usability of the code through the usage of functions in python for sequential execution

--- Lambda/processA.py
+++ Lambda/processA.py
@@ -1,16 +1,26 @@
 import requests
 import json
 
-function_url = 'https://pdx3axs6nfkww2hia2irl55bwq0oubzi.lambda-url.us-east-1.on.aws/'
+def executeProcess(function_url, payload, specifier):
+    response = requests.post(function_url, data=json.dumps(payload))
+
+    if response.status_code == 200:
+        print(f"Post sent successfully | Master-thread: {specifier}")
+        print(response.json())
+    else:
+        print("ERROR: Process was not executable")
+        print(f"Reason: {response.status_code}")
+
+    print("\n")
+
+
+function_urlMasterA = 'https://pdx3axs6nfkww2hia2irl55bwq0oubzi.lambda-url.us-east-1.on.aws/'
+function_urlMasterB = 'https://pdx3axs6nfkww2hia2irl55bwq0oubzi.lambda-url.us-east-1.on.aws/'
+function_urlMasterC = 'https://pdx3axs6nfkww2hia2irl55bwq0oubzi.lambda-url.us-east-1.on.aws/'
 
 payload = {
 }
 
-response = requests.post(function_url, data=json.dumps(payload))
-
-if response.status_code == 200:
-    print("Post sent successfully")
-    print(response.json())
-else:
-    print("ERROR: Process was not executable")
-    print(f"Reason: {response.status_code}")
+executeProcess(function_urlMasterA, payload, "A") # Execution of A Master Thread
+executeProcess(function_urlMasterB, payload, "B") # Execution of B Master Thread
+executeProcess(function_urlMasterC, payload, "C") # Execution of C Master Thread
